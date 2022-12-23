17c61f1e99e7e8ded823ad098b128e7494c45d95
islam saman <islam.saman4@gmail.com>
2022-12-23 18:49:51 +0200

    InsertTable Function

--- table.sh
+++ table.sh
@@ -98,6 +98,99 @@ echo $choice
 	            echo "The name is wrong, the name don't have to start with numbers or special character and not contain spaces or special character"
             fi
         done            
-        ;;        
+        ;;   
+
+        InsertIntoTable )
+        while true
+        do
+            read -p "Enter Name of the table to insert into : " name
+            if [[ ${name} =~ $regex ]]
+                then
+                    if [ -e $name ] 
+                        then
+
+
+
+                            while (( $count <= $fiel ))
+                            do
+                                colum_data=$(sed -n '1p' t8 | cut -d: -f$count)
+                                colum_type=$(sed -n '2p' t8 | cut -d: -f$count)
+
+                                read -p "Enter the $var : " user_input
+                                
+                                if [[ $var2 == "int" ]]
+                                    then
+                                        if [[ ${user_input} =~ $intReg ]]
+                                            then
+                                                str+=$user_input":"
+                                        else
+                                            continue
+                                        fi
+                                else
+                                    if [[ ${user_input} =~ $strReg ]]
+                                        then 
+                                            str+=$user_input":"
+                                    else
+                                        continue
+                                    fi    
+                                fi
+
+                                let count=$count+1
+                            done                       
+
+
+
+
+
+
+
+                            echo "Table Already Exits "
+                            read -p "Do you want to Enter insert something else (yes or no)" user_input
+                            if [[ $user_input == "yes" ]]
+                                then 
+                                    continue
+                            else
+                                break
+                            fi
+                    else 
+                        touch $name
+                        read -p "Enter Number of Fields : " num_Field
+                        echo
+                        echo "The first colum is PRIMAY KEY by defualt"
+
+                        declare -i num_Fields=$num_Field
+                        declare -i count=1
+                        colum=""
+                        type=""
+
+                        while (( $count <= $num_Fields ))
+                        do
+                            if [[ $count == $num_Fields ]]
+                                then
+                                    read -p "Enter the $count Field : " user_content
+                                    colum+=$user_content
+
+                                    read -p "Enter the type of field : " content_type
+                                    type+=$content_type
+                            else
+                                    read -p "Enter the $count Field : " user_content
+                                    colum+=$user_content":"
+
+                                    read -p "Enter the type of field : " content_type
+                                    type+=$content_type":"                                  
+                            fi
+                            let count=$count+1
+                        done
+                        echo $colum >> $name
+                        echo $type >> $name
+
+                        break
+                    fi             	    
+             else
+	            echo "The name is wrong, the name don't have to start with numbers or special character and not contain spaces or special character"
+            fi
+        done
+        ;;  
+
         esac
 done
