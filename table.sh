6c1dd0191e6abe9a699231572b6091584866b1bc
islam saman <islam.saman4@gmail.com>
2022-12-23 14:57:14 +0200

    Create Table

--- table.sh
+++ table.sh
@@ -4,4 +4,37 @@ shopt -s extglob
 regex='^[A-Z | a-z][A-Za-z0-9]+$'
 
 
-echo "This work"
+select choice in CreateTable ListTables DropTable InsertIntoTable SelectFromTable DeleteFromTable UpdateTable
+do 
+
+echo $choice
+
+    case $choice in 
+        CreateTable )
+        while true
+        do
+            read -p "Enter Name of your Table : " name
+            if [[ ${name} =~ $regex ]]
+                then
+                    if [ -e $name ] 
+                        then
+                            echo "Table Already Exits "
+                            read -p "Do you want to Enter the name Again(yes or no)" user_input
+
+                        if [[ $user_input == "yes" ]]
+                            then 
+                                continue
+                        else
+                            break
+                        fi
+                    else 
+                        touch $name
+                        break
+                    fi             	    
+             else
+	            echo "The name is wrong, the name don't have to start with numbers or special character and not contain spaces or special character"
+            fi
+        done
+        ;;
+        esac
+done
